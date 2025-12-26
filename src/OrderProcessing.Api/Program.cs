874395452749ac8780237fb75eddf33322d829fb
Alexandru <ademciucen@gmail.com>
2025-12-26 18:25:16 +0200

    Configurare Scalar pentru OpenAPI UI

--- src/OrderProcessing.Api/Program.cs
+++ src/OrderProcessing.Api/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddOpenApi();
 
 // Register repositories
 builder.Services.AddScoped<IProductsRepository, InMemoryProductsRepository>();
@@ -21,8 +21,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
