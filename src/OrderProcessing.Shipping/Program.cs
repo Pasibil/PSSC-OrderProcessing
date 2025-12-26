2b42c4a7522dafe01eacfee7bcf6343e57c1f4fd
Alexandru <ademciucen@gmail.com>
2025-12-26 19:26:02 +0200

    Adauga Swagger UI la toate microserviciile
    - Instalat NSwag.AspNetCore la Invoicing si Shipping
    - Configurat AddOpenApiDocument cu titluri descriptive
    - Toate microserviciile au acum interfata Swagger
    - Accesibile la /swagger pe fiecare port

--- src/OrderProcessing.Shipping/Program.cs
+++ src/OrderProcessing.Shipping/Program.cs
@@ -3,14 +3,19 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddOpenApi();
+builder.Services.AddOpenApiDocument(config =>
+{
+    config.Title = "Shipping API";
+    config.Version = "v1";
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseOpenApi();
+    app.UseSwaggerUi();
 }
 
 app.UseHttpsRedirection();
